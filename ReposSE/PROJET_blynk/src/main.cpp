ca899eb011d1be64f086f434dbfb8653a97d08f4
ChristVOUFO <HE304256@students.ephec.be>
2022-12-14 17:02:21 +0100

    Suite

---
+++ ReposSE/PROJET_blynk/src/main.cpp
@@ -0,0 +1,196 @@
+/*#define BLYNK_TEMPLATE_ID "TMPLz7sUh_-y"
+#define BLYNK_DEVICE_NAME "TP5"
+#define BLYNK_AUTH_TOKEN "PNiTyglWPiT04yHCCSzLQgGDaAvrcEg3"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Christ";
+char pass[] = "1234ABCD";
+
+// Select your pin with physical button
+const int btnPin = 12;
+
+WidgetLED led3(V3);
+
+BlynkTimer timer;
+
+// V3 LED Widget represents the physical button state
+boolean btnState = false;
+void buttonLedWidget()
+{
+  // Read button
+  boolean isPressed = (digitalRead(btnPin) == LOW);
+
+  // If state has changed...
+  if (isPressed != btnState) {
+    if (isPressed) {
+      led3.on();
+    } else {
+      led3.off();
+    }
+    btnState = isPressed;
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass, IPAddress(192,168,137,10), 8080);
+
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup physical button pin (active low)
+  pinMode(btnPin, INPUT_PULLUP);
+
+  timer.setInterval(500L, buttonLedWidget);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}*/
+
+/*#define BLYNK_TEMPLATE_ID           "TMPLqvDjP2xJ"
+#define BLYNK_DEVICE_NAME           "ESP32"
+#define BLYNK_AUTH_TOKEN            "Znp49CU6qG-IS9sWcNyKrHyuFu8DwmT1"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Christ";
+char pass[] = "1234ABCD";
+
+// Select your pin with physical button
+const int btnPin = 13;
+
+WidgetLED led3(V3);
+
+BlynkTimer timer;
+
+// V3 LED Widget represents the physical button state
+boolean btnState = false;
+void buttonLedWidget()
+{
+  // Read button
+  boolean isPressed = (digitalRead(btnPin) == LOW);
+
+  // If state has changed...
+  if (isPressed != btnState) {
+    if (isPressed) {
+      led3.on();
+    } else {
+      led3.off();
+    }
+    btnState = isPressed;
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass, IPAddress(192,168,137,210), 8080);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup physical button pin (active low)
+  pinMode(btnPin, INPUT_PULLUP);
+
+  timer.setInterval(500L, buttonLedWidget);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}*/
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <Arduino.h>
+
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+
+// Update these with values suitable for your network.
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+
+#define BLYNK_TEMPLATE_ID "TMPL4Gs24xgm"
+#define BLYNK_DEVICE_NAME "NodeMCU"
+#define BLYNK_AUTH_TOKEN "VpIleLjs4RNBNXWEWMz64bdoyNyypl-s"
+
+int pin = 2;
+
+const char* auth = BLYNK_AUTH_TOKEN;
+const char* ssid = "Christ";
+const char* pass = "1234ABCD";
+const char* mqtt_server = "10.22.1.171";
+
+//Récupérer état du bouton
+BLYNK_WRITE(V0){
+  digitalWrite(pin, param.asInt());
+} 
+void setup() {
+  pinMode(pin, OUTPUT);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin("Your auth. Token", ssid, pass);
+  Serial.begin(115200);
+  //setup_wifi();
+  client.setServer(mqtt_server, 1883);
+
+ /*if ((char)payload[0] == 'O' && (char)payload[1] == 'N') {
+    digitalWrite(BUILTIN_LED, HIGH);   
+  } 
+  else if ((char)payload[0] == 'O' && (char)payload[1] == 'F' && (char)payload[2] == 'F')
+  {
+    digitalWrite(BUILTIN_LED, LOW);
+  }*/
+
+}
+
+void loop() {
+  client.loop();
+  Blynk.run();
+
+    DynamicJsonDocument doc(256);
+
+    doc["BP"] = digitalRead(V0) ? "ACTIF" : "NOT ACTIF";
+    doc["LED"] = digitalRead(pin) ? "ON" : "OFF";
+  
+  char json_string[256];
+  serializeJson(doc, json_string);
+  client.publish("Blynk", json_string);
+}
--- ReposSE/TP6/src/main.cpp
+++ ReposSE/TP6/src/main.cpp
@@ -1,11 +1,15 @@
+#include <DHT.h>
 #include <WiFi.h>
+#include <Arduino.h>
+#include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
 
 // Update these with values suitable for your network.
 
-const char* ssid = "........";
-const char* password = "........";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* ssid = "Christ";
+const char* password = "1234ABCD";
+const char* mqtt_server = "10.22.1.145";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -13,6 +17,17 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE	(50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
+int sensorValue;
+
+// DHT 11 sensor
+#define DHTPIN 26
+#define DHTTYPE DHT11
+#define PIN_RED    23 // GIOP23
+#define PIN_GREEN  19 // GIOP22
+#define PIN_BLUE   18 // GIOP21
+#define BUTTON_PIN 12 // GIOP21 pin connected to button
+// DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup_wifi() {
 
@@ -43,18 +58,56 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
+    analogWrite(PIN_GREEN, HIGH);
+    Serial.print((char)payload[i]);
+  }
+  for (int i = 0; i > length; i--) {
+    digitalWrite(PIN_GREEN, LOW);
     Serial.print((char)payload[i]);
   }
   Serial.println();
 
   // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  if ((char)payload[0] == 'O' && (char)payload[1] == 'N') {
+    digitalWrite(BUILTIN_LED, HIGH);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  } 
+  else if ((char)payload[0] == 'O' && (char)payload[1] == 'F' && (char)payload[2] == 'F')
+  {
+    digitalWrite(BUILTIN_LED, LOW);
   }
+   
+  /*if (payload == payload ) {
+    digitalWrite(PIN_RED, HIGH);
+    digitalWrite(PIN_GREEN, HIGH);
+    digitalWrite(PIN_BLUE, HIGH);
+
+  } 
+   else 
+  {
+    digitalWrite(PIN_RED, LOW); 
+    digitalWrite(PIN_GREEN, LOW);
+    digitalWrite(PIN_BLUE, LOW);
+  }
+
+  if (payload > 0) {
+    digitalWrite(PIN_GREEN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } 
+  else {
+    digitalWrite(PIN_GREEN, LOW);  // Turn the LED off by making the voltage HIGH
+  }
+
+  if (payload > 0) {
+    digitalWrite(PIN_BLUE, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } 
+  else {
+    digitalWrite(PIN_BLUE, LOW);  // Turn the LED off by making the voltage HIGH
+  }*/
 
 }
 
@@ -71,7 +124,9 @@ void reconnect() {
       // Once connected, publish an announcement...
       client.publish("outTopic", "hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      client.subscribe("LED");
+      client.subscribe("RGB");
+      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -83,6 +138,12 @@ void reconnect() {
 }
 
 void setup() {
+  
+  dht.begin();
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
@@ -91,19 +152,37 @@ void setup() {
 }
 
 void loop() {
-
+  
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
+  
+  // Grab the current state of the sensor
+  //int humidity_data = dht.readHumidity();
+  //int temperature_data = dht.readTemperature();
   unsigned long now = millis();
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    /*snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
     Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
-  }
-}
+    Serial.println(msg);*/
+    
+    int humidity = dht.readHumidity();
+    int temperature = dht.readTemperature();
+    sensorValue = analogRead(39); // read analog input pin 0
+
+    DynamicJsonDocument doc(256);
+
+    doc["BP"] = digitalRead(BUTTON_PIN) ? "ACTIF" : "NOT ACTIF";
+    doc["TEMPERATURE"] = temperature;
+    doc["HUMIDITY"] = humidity;
+    doc["LDR"] = sensorValue;
+  
+  char json_string[256];
+  serializeJson(doc, json_string);
+  client.publish("Gauge", json_string);
+  
+
+ }
